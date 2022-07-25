d9d005037be1c233dc8d4ab19e41fec437ae5685
n1c0145 <nicolas.lozalml@gmail.com>
2022-07-25 15:49:41 -0500

    Configuring port

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log('stating project');
+const express = require('express');
+const morgan = require('morgan');
+const db = require('./database/database');
+const path = require('path');
+
+const app = express();
+
+// Settings 
+app.set('port', process.env.PORT || 3000);
+
+// Starting the server
+app.listen(app.get('port'), () => {
+    console.log(`Server on port ${app.get('port')}`);
+  });
